b205a583dc207748240fe96132d2dbc706862aa3
Dev-Beom <youngbeom0908@gmail.com>
2021-07-18 02:05:19 +0900

    study variables
    let, const, var ... etc

---
+++ modern_javascript_tutorial/1_variables.js
@@ -0,0 +1,14 @@
+// 1. 변수 가지고 놀기
+let admin;
+let name;
+
+name = "John";
+admin = name;
+console.log(name);
+
+
+// 2. 대문자 상수 올바로 사용하기
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+console.log(COLOR_RED, COLOR_GREEN, COLOR_BLUE);
